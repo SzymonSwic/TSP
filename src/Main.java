88d9d961c1b1ed0dc272873c5840ef4a5810943b
Szymon <szymonswic@gmail.com>
2019-10-05 17:00:47 +0200

    src files fix

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
-import ExperimentEnv.Population;
 import ExperimentEnv.TSPProblem;
 import ExperimentEnv.TSPProblemCreator;
 
 public class Main {
 
     public static void main(String[] args) {
-        TSPProblemCreator creator = new TSPProblemCreator("TSP/berlin11_modified.tsp");
+        TSPProblemCreator creator = new TSPProblemCreator("TSP/pr2392.tsp");
         TSPProblem tspProblem = creator.create();
 
-        Algorithm algorithm = new Algorithm();
-        algorithm.start();
+//        Algorithm algorithm = new Algorithm();
+//        algorithm.start();
+
+        tspProblem.displayTSPProblem();
     }
 }
